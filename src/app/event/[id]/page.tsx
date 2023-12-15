d58a34bcdcda214d2c2226ce03e516a665e23e11
Thiago de Oliveira Cordeiro <thiagoty52@gmail.com>
2023-12-15 13:59:47 -0300

    :sparkles: feat: created event page

--- src/app/event/[id]/page.tsx
+++ src/app/event/[id]/page.tsx
@@ -5,7 +5,17 @@ type Props = {
 };
 
 function Page({ params }: Props) {
-  return <div>ID do evento: {params.id}</div>;
+  return (
+    <main className="text-center mx-auto max-w-lg p-5">
+      <header>
+        <h2 className="text-2xl text-yellow-400">Amigo Secreto</h2>
+        <h1 className="text-3xl mt-5 mb-2">** Título do evento **</h1>
+        <p className="text-sm mb-5">** Descrição do evento **</p>
+      </header>
+
+      <footer className="mt-5 text-sm">Criado por Thiago Cordeiro</footer>
+    </main>
+  );
 }
 
 export default Page;
