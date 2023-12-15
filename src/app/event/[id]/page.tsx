91dbbb05a338ce9d759a487757eece352ab808fa
Thiago de Oliveira Cordeiro <thiagoty52@gmail.com>
2023-12-15 16:12:11 -0300

    :sparkles: feat: added logic to query event data

--- src/app/event/[id]/page.tsx
+++ src/app/event/[id]/page.tsx
@@ -1,16 +1,23 @@
+import * as api from "@/api/site";
+import { redirect } from "next/navigation";
+
 type Props = {
   params: {
     id: string;
   };
 };
 
-function Page({ params }: Props) {
+async function Page({ params }: Props) {
+  const eventItem = await api.getEvent(Number(params.id));
+
+  if (!eventItem || !eventItem.status) return redirect("/");
+
   return (
     <main className="text-center mx-auto max-w-lg p-5">
       <header>
         <h2 className="text-2xl text-yellow-400">Amigo Secreto</h2>
-        <h1 className="text-3xl mt-5 mb-2">** Título do evento **</h1>
-        <p className="text-sm mb-5">** Descrição do evento **</p>
+        <h1 className="text-3xl mt-5 mb-2">{eventItem.title}</h1>
+        <p className="text-sm mb-5">{eventItem.description}</p>
       </header>
 
       <footer className="mt-5 text-sm">Criado por Thiago Cordeiro</footer>
---
+++ src/types/Event.ts
@@ -0,0 +1,7 @@
+export type Event = {
+  description: string;
+  grouped: boolean;
+  id: number;
+  status: boolean;
+  title: string;
+};
